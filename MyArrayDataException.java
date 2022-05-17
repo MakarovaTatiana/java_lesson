150ebc4fdc3c5301a53249798cadcf95460b32e9
geroitrueda <91047066+geroitrueda@users.noreply.github.com>
2022-05-17 17:18:56 +0500

    Create MyArrayDataException.java
    1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4. При подаче массива другого размера необходимо бросить исключение MyArraySizeException.
    2. Далее метод должен пройтись по всем элементам массива, преобразовать в int и просуммировать. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException с детализацией, в какой именно ячейке лежат неверные данные.
    3. В методе main() вызвать полученный метод, обработать возможные исключения MyArraySizeException и MyArrayDataException и вывести результат расчета.

---
+++ MyArrayDataException.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class MyArrayDataException extends Exception {
+    public int i;
+    public int j;
+
+    MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
+
