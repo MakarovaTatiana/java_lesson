eb58e65cc44a92965f98a055e33e7282da20f68a
geroitrueda <91047066+geroitrueda@users.noreply.github.com>
2022-06-12 22:16:45 +0500

    Create Course.java
    Имеется следующая структура:
       interface Student {
            String getName();
            List<Course> getAllCourses();
        }
        interface Course {}
    1. Написать функцию, принимающую список Student и возвращающую список уникальных курсов, на которые подписаны студенты.
    2. Написать функцию, принимающую на вход список Student и возвращающую список из трех самых любознательных (любознательность определяется количеством курсов).
    3. Написать функцию, принимающую на вход список Student и экземпляр Course, возвращающую список студентов, которые посещают этот курс.

---
+++ Course.java
@@ -0,0 +1,36 @@
+package lesson8;
+
+import java.util.Objects;
+
+public class Course {
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Курс " +
+                name ;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(name, course.name);
+    }
+}
