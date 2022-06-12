66311fdd95cb03d8eb0f956ba69c6756a75fa7db
geroitrueda <91047066+geroitrueda@users.noreply.github.com>
2022-06-13 00:19:18 +0500

    Create Main.java
    1. Добавить поддержку SQLite в проект.
    2. Создать класс-репозиторий, отвечающий за взаимодействие с базой данных.
    3. Организовать запись данных в базу при каждом успешном API запросе. Формат - String city, String localDate, String weatherText, Double temperature.
    4. Организовать чтение из базы всех данных по пункту меню (требует переработки меню)
    5. Учесть, что соединение всегда нужно закрывать

---
+++ Main.java
@@ -0,0 +1,10 @@
+package lesson9;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
